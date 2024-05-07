cdd4e6168dd84f8b2f435df5f8c681ff127c7d94
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-05-07 18:40:49 +0800

    Update 咕咕番.js

--- JS/咕咕番.js
+++ JS/咕咕番.js
@@ -3,18 +3,37 @@ var rule = {
      host: 'https://www.gugufan.com',
      模板:'短视2',
      searchUrl: '/index.php/vod/search/wd/**.html',
-     //url: '/index.php/vod/show/id/fyclass.html',
      url:'/index.php/api/vod#type=fyclass&page=fypage',
      searchable: 2,//是否启用全局搜索,
      quickSearch: 1,//是否启用快速搜索,
      filterable: 0,//是否启用分类筛选,
      headers: {
-       'User-Agent': 'MOBILE_UA'
+       'User-Agent': 'MOBILE_UA',
+       'Cookie': 'PHPSESSID=fa32urmgd2slnh8f4k8rgrebci; ecPopup=1; user_id=167674; user_name=zhulr77; group_id=2; group_name=%E8%90%8C%E6%96%B0; user_check=8dd17f0db0477d573f6a48b6469ee8c8; user_portrait=%2Fstatic%2Fimages%2Ftouxiang.png'
      },
      class_name:'连载新番&完结动画&动漫电影&特摄动画',
      class_url:'6&7&21&23',
      play_parse: true,
-     lazy: '',
+     lazy:`js:
+        var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
+        var url = html.url;
+        var from = html.from;
+        var MacPlayerConfig={};
+        eval(fetch(HOST + "/static/js/playerconfig.js").replace('var Mac','Mac'));
+        if (/.m3u8|.mp4/.test(url)) {
+            input = url
+        } else {
+        var list = MacPlayerConfig.player_list[from].parse;
+            input={
+                jx:0,
+                url:list+url,
+                parse:1,
+                header: JSON.stringify({
+                    'referer': HOST
+                })
+            }
+        }
+     `,
      limit: 6,
      推荐: '.public-list-exp;a&&title;img&&data-src;.ft2&&Text;a&&href',
      double: false, // 推荐内容是否双层定位
